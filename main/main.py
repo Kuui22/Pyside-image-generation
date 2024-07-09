3eafb30e19195970635356f3e633840c1aab778c
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-07-09 09:03:47 +0200

    build project
    initial build

---
+++ main/main.py
@@ -0,0 +1,17 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+from ui_mainwindow import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self._ui = Ui_MainWindow()
+        self._ui.setupUi(self)
+
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
