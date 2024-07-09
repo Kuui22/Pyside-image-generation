3a54416e9f632e7f1957d620a04f6a725014318b
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-07-09 09:48:17 +0200

    created main window
    with working button

--- main/main.py
+++ main/main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-
+from PySide6.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
 
 
@@ -8,7 +8,14 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self._ui = Ui_MainWindow()
-        self._ui.setupUi(self)
+        self._ui.setupUi(self) 
+        self._ui.okButton.clicked.connect(self.say_hello)
+
+    @Slot()
+    def say_hello(self):
+        text = self._ui.promptText.toPlainText()
+        print(text)
+
 
 
 if __name__ == '__main__':
@@ -16,3 +23,4 @@ if __name__ == '__main__':
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+
--- main/ui_mainwindow.py
+++ main/ui_mainwindow.py
@@ -11,27 +11,44 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
-    QStatusBar, QWidget)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMenu, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QTextEdit,
+    QWidget)
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(800, 600)
+        self.actionExit = QAction(MainWindow)
+        self.actionExit.setObjectName(u"actionExit")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.okButton = QPushButton(self.centralwidget)
+        self.okButton.setObjectName(u"okButton")
+        self.okButton.setGeometry(QRect(360, 440, 75, 24))
+        self.promptText = QTextEdit(self.centralwidget)
+        self.promptText.setObjectName(u"promptText")
+        self.promptText.setGeometry(QRect(83, 120, 311, 61))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        self.menubar.setEnabled(True)
+        self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        self.menuFile = QMenu(self.menubar)
+        self.menuFile.setObjectName(u"menuFile")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menuFile.addAction(self.actionExit)
 
         self.retranslateUi(MainWindow)
 
@@ -39,6 +56,10 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"GenQT", None))
+        self.actionExit.setText(QCoreApplication.translate("MainWindow", u"Exit", None))
+        self.okButton.setText(QCoreApplication.translate("MainWindow", u"Send", None))
+        self.promptText.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Prompt here!", None))
+        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
     # retranslateUi
 
